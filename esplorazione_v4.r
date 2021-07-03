00880467649cc94acd432e8583349d3c6ac9f92a
lorenzlorg <lorenzlorg@gmail.com>
2021-07-03 17:42:58 +0200

    aggiunti zeri per ogni ristorante nei periodi no covid

--- esplorazione_v4.r
+++ esplorazione_v4.r
@@ -133,31 +133,37 @@ col_nomi <- c("data", "data_anno_prec", "vendite", "scontrini")
 #ristorante1
 ristorante1 <- data.frame(col_date, ristorazione$vendite1, ristorazione$scontrini1)
 colnames(ristorante1) <- col_nomi
+ristorante1$vendite[is.na(ristorante1$vendite)] <- 0  # sostiuisco i valori NA con 0
 ristorante1$rapprto_v_s <- ristorante1$vendite/ristorante1$scontrini
 
-#ristorante4
-ristorante4 <- data.frame(col_date, ristorazione$vendite2, ristorazione$scontrini2)
-colnames(ristorante4) <- col_nomi
-ristorante4$rapprto_v_s <- ristorante4$vendite/ristorante4$scontrini
+#ristorante2
+ristorante42 <- data.frame(col_date, ristorazione$vendite2, ristorazione$scontrini2)
+colnames(ristorante2) <- col_nomi
+ristorante2$vendite[is.na(ristorante2$vendite)] <- 0 
+ristorante2$rapprto_v_s <- ristorante2$vendite/ristorante2$scontrini
 
 #ristorante3
 ristorante3 <- data.frame(col_date, ristorazione$vendite3, ristorazione$scontrini3)
 colnames(ristorante3) <- col_nomi
+ristorante3$vendite[is.na(ristorante3$vendite)] <- 0 
 ristorante3$rapprto_v_s <- ristorante3$vendite/ristorante3$scontrini
 
 #ristorante4
 ristorante4 <- data.frame(col_date,ristorazione$vendite4, ristorazione$scontrini4)
 colnames(ristorante4) <- col_nomi
+ristorante4$vendite[is.na(ristorante4$vendite)] <- 0  
 ristorante4$rapprto_v_s <- ristorante4$vendite/ristorante4$scontrini
 
 #ristorante5
 ristorante5 <- data.frame(col_date, ristorazione$vendite5, ristorazione$scontrini5)
 colnames(ristorante5) <- col_nomi
+ristorante5$vendite[is.na(ristorante5$vendite)] <- 0 
 ristorante5$rapprto_v_s <- ristorante5$vendite/ristorante5$scontrini
 
 #ristorante6
 ristorante6 <- data.frame(col_date, ristorazione$vendite6, ristorazione$scontrini6)
 colnames(ristorante6) <- col_nomi
+ristorante6$vendite[is.na(ristorante6$vendite)] <- 0  
 ristorante6$rapprto_v_s <- ristorante6$vendite/ristorante6$scontrini
 
 # rimuovo le variabili che mi sono servite nella fase sopra
@@ -187,6 +193,12 @@ abline(h=mean(as.integer(ristorante5$vendite)))
 plot(ristorante6$data, ristorante6$vendite, xlab = "data", ylab = "vendite", type="l", main = "Ristorante 6")
 abline(h=mean(as.integer(ristorante6$vendite)))
 # è possibile notare la presenza di valori NA
+
+
+
+
+
+
 
 # decomposizione serie primo ristorante
 vendite1 <- ristorante1[, 4]
