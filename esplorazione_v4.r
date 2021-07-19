5a9b61e6c842e1394bd2240a12d49859c2c13465
lorenzlorg <lorenzlorg@gmail.com>
2021-07-19 15:34:11 +0200

    aggiunto primo grafico per confronto estati

--- esplorazione_v4.r
+++ esplorazione_v4.r
@@ -669,20 +669,38 @@ arima1
 
 # seleziono un periodo temporale che corrisponde all'estate
 # ricavo due serie storiche, per 2019 e 2020
-inizio_estate_2019 <- as.Date("2019-06-21", format = "%Y-%m-%d")
-fine_estate_2019 <- as.Date("2019-09-23", format = "%Y-%m-%d")
 
-inizio_estate_2020 <- as.Date("2020-06-20", format = "%Y-%m-%d")
+inizio_estate_2019 <- as.Date("2019-06-21", format = "%Y-%m-%d")
+fine_estate_2019 <- as.Date("2019-09-22", format = "%Y-%m-%d")
+
+inizio_estate_2020 <- as.Date("2020-06-21", format = "%Y-%m-%d")
 fine_estate_2020 <- as.Date("2020-09-22", format = "%Y-%m-%d")
 
 
 ristorante1_estate_2019 <- subset(ristorante1, data >= inizio_estate_2019 & data <= fine_estate_2019)
-ristorante1_estate_2020 <- subset(ristorante1, data >= inizio_estate_2020 & data <= fine_estate_2020)
+ristorante1_estate_2019 <- ristorante1_estate_2019[,-2]
 
-# scale per essere sulla stessa scala e percepire meglio le differenze
-par(mfrow=c(1,2))
-plot(ristorante1_estate_2019$data, scale(ristorante1_estate_2019$vendite), xlab = "data", ylab = "vendite", type="l", main = "Ristorante 1 - estate 2019")
-plot(ristorante1_estate_2020$data, scale(ristorante1_estate_2020$vendite), xlab = "data", ylab = "vendite", type="l", main = "Ristorante 1 - estate 2020")
+ristorante1_estate_2020 <- subset(ristorante1, data >= inizio_estate_2020 & data <= fine_estate_2020)
+ ristorante1_estate_2020 <- ristorante1_estate_2020[,-2]
+
+ristoranti_unione_2019_2020 <- rbind(ristorante1_estate_2019, ristorante1_estate_2020)
+
+ristoranti_unione_2019_2020$Year <- format(ristoranti_unione_2019_2020$data, "%Y")
+ristoranti_unione_2019_2020$Month <- format(ristoranti_unione_2019_2020$data, "%b")
+ristoranti_unione_2019_2020$Day <- format(ristoranti_unione_2019_2020$data, "%d")
+
+ristoranti_unione_2019_2020$MonthDay <- format(ristoranti_unione_2019_2020$data, "%d-%b")
+ristoranti_unione_2019_2020$MonthDay2 <- ristoranti_unione_2019_2020$MonthDay
+
+ristoranti_unione_2019_2020$MonthDay2[as.numeric(row.names(ristoranti_unione_2019_2020))%%3!=0] <- ""
+labels <- ristoranti_unione_2019_2020$MonthDay2
+
+p <- ggplot(data=ristoranti_unione_2019_2020, mapping=aes(x=MonthDay, y=vendite, shape=Year, color=Year)) + geom_point() +geom_line(aes(group = 1))
+p <- p + facet_grid(facets = Year ~ ., margins = FALSE) + theme_bw()
+p + scale_y_continuous() + scale_x_discrete(labels=labels) + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, size = 8))
+
+
 
 # si procede con un'analisi più approfondita e più tencica del grafico per rispondere 
 # alla domanda di ricerca: ovvero nel 2020 c'era pochissime restrizioni, quasi 
