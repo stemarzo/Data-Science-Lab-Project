438705fb78fce68cec917d4aa795d0313093e6f1
lorenzlorg <lorenzlorg@gmail.com>
2021-07-21 17:43:00 +0200

    correzione calori zero per scontrini, vendite e rapporto vendite scontrini

--- restaurant_analysis.r
+++ restaurant_analysis.r
@@ -333,37 +333,50 @@ col_nomi <- c("data", "data_anno_prec", "vendite", "scontrini")
 ristorante1 <- data.frame(col_date, ristorazione$vendite1, ristorazione$scontrini1)
 colnames(ristorante1) <- col_nomi
 ristorante1$vendite[is.na(ristorante1$vendite)] <- 0  # sostiuisco i valori NA con 0 (per alcuni casi si potrebbe sostituire con valori quali la media)
+ristorante1$scontrini[is.na(ristorante1$scontrini)] <- 0  
 ristorante1$rapprto_v_s <- ristorante1$vendite/ristorante1$scontrini
+ristorante1$rapprto_v_s[is.na(ristorante1$rapprto_v_s)] <- 0
 
 #ristorante2
 ristorante2 <- data.frame(col_date, ristorazione$vendite2, ristorazione$scontrini2)
 colnames(ristorante2) <- col_nomi
 ristorante2$vendite[is.na(ristorante2$vendite)] <- 0 
+ristorante2$scontrini[is.na(ristorante2$scontrini)] <- 0  
 ristorante2$rapprto_v_s <- ristorante2$vendite/ristorante2$scontrini
+ristorante2$rapprto_v_s[is.na(ristorante2$rapprto_v_s)] <- 0
+
 
 #ristorante3
 ristorante3 <- data.frame(col_date, ristorazione$vendite3, ristorazione$scontrini3)
 colnames(ristorante3) <- col_nomi
 ristorante3$vendite[is.na(ristorante3$vendite)] <- 0 
+ristorante3$scontrini[is.na(ristorante3$scontrini)] <- 0  
 ristorante3$rapprto_v_s <- ristorante3$vendite/ristorante3$scontrini
+ristorante3$rapprto_v_s[is.na(ristorante3$rapprto_v_s)] <- 0
 
 #ristorante4
 ristorante4 <- data.frame(col_date,ristorazione$vendite4, ristorazione$scontrini4)
 colnames(ristorante4) <- col_nomi
 ristorante4$vendite[is.na(ristorante4$vendite)] <- 0  
+ristorante4$scontrini[is.na(ristorante4$scontrini)] <- 0  
 ristorante4$rapprto_v_s <- ristorante4$vendite/ristorante4$scontrini
+ristorante4$rapprto_v_s[is.na(ristorante4$rapprto_v_s)] <- 0
 
 #ristorante5
 ristorante5 <- data.frame(col_date, ristorazione$vendite5, ristorazione$scontrini5)
 colnames(ristorante5) <- col_nomi
 ristorante5$vendite[is.na(ristorante5$vendite)] <- 0 
+ristorante5$scontrini[is.na(ristorante5$scontrini)] <- 0  
 ristorante5$rapprto_v_s <- ristorante5$vendite/ristorante5$scontrini
+ristorante5$rapprto_v_s[is.na(ristorante5$rapprto_v_s)] <- 0
 
 #ristorante6
 ristorante6 <- data.frame(col_date, ristorazione$vendite6, ristorazione$scontrini6)
 colnames(ristorante6) <- col_nomi
 ristorante6$vendite[is.na(ristorante6$vendite)] <- 0  
+ristorante6$scontrini[is.na(ristorante6$scontrini)] <- 0  
 ristorante6$rapprto_v_s <- ristorante6$vendite/ristorante6$scontrini
+ristorante6$rapprto_v_s[is.na(ristorante6$rapprto_v_s)] <- 0
 
 # rimuovo le variabili che mi sono servite nella fase sopra
 rm(list = c('col_date','col_nomi'))
