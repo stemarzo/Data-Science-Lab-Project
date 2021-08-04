6a3fdd177341c8e6d914cb39c78d365f8339215a
lorenzlorg <lorenzlorg@gmail.com>
2021-08-04 09:53:44 +0200

    test

--- esperimenti_lor.R
+++ esperimenti_lor.R
@@ -1,101 +1,36 @@
 # WORKING IN PROGRESS #
 
-# considero la serie storica relativa al primo ristorante
 
-# tentativo 1 ----
-vendite1_day_pre
-arima1 <- auto.arima(vendite1_day_pre)
-arima1
+# lisciamento esponenziale ----
 
-train_index <- 800
-n_total <- nrow(ristorante1_pre_covid)
-rist1_vendite_train1 <- ristorante1_pre_covid[1:(train_index),]
-rist1_vendite_test <- ristorante1_pre_covid[(train_index+1):n_total,]
-predicted <- numeric(n_total-train_index)
-
-
-for (i in 1:(n_total-train_index)) {
-  shampoo_sales_train <- ristorante1_pre_covid[1:(train_index-1+i),]
-  arima_model <- auto.arima(vendite1_day_pre)
-  pred <- forecast(arima_model, 1)
-  predicted[i] <- pred$mean
-}
-
-
-df_pred <- tibble(obs = c(rist1_vendite_train1$vendite, rist1_vendite_test$vendite), 
-                  predicted = c(rist1_vendite_train1$vendite, predicted), 
-                  time = ristorante1_pre_covid$data) 
-
-
-
-ggplot(gather(df_pred, obs_pred, value, -time) %>% 
-         mutate(obs_pred = factor(obs_pred, levels = c("predicted", "obs"))), 
-       aes(x = time, y = value, col = obs_pred, linetype = obs_pred)) +
-  geom_line() +
-  xlab("") + ylab("") +
-  scale_color_manual(values=c("black", "hotpink")) +
-  scale_linetype_manual(values=c(2, 1)) +
-  scale_x_date(date_labels = "%y %b", date_breaks = "2 month") +
-  theme_bw() + theme(legend.title = element_blank(),
-                     axis.text.x  = element_text(angle=45, vjust=0.5))
-
-# tentativo 2 ----
-tsData <- vendite1_day_pre
-
-components.ts = decompose(tsData)
-plot(components.ts)
-
-library("fUnitRoots")
-par(mfrow=c(1,1))
-
-urkpssTest(tsData, type = c("tau"), lags = c("short"),use.lag = NULL, doplot = TRUE)
-tsstationary = diff(tsData, differences=1)
-plot(tsstationary)
-
-
-
-acf(tsData,lag.max=34) 
-
-
-timeseriesseasonallyadjusted <- tsData- components.ts$seasonal
-tsstationary <- diff(timeseriesseasonallyadjusted, differences=1)
-
-acf(tsstationary, lag.max=34)
-pacf(tsstationary, lag.max=34)
-
-
-fitARIMA <- auto.arima(tsData, trace=FALSE) 
-
-
-predict(fitARIMA,n.ahead = 5)
-library("fUnitRoots")
-library(lmtest)
-library(forecast)
-library(FitAR)
-futurVal <- forecast.Arima(fitARIMA,h=10, level=c(99.5))
-plot.forecast(futurVal)
-
-# tentativo 3 ----
-
-library('ggplot2')
-library('forecast')
-library('tseries')
-
-ggplot(ristorante1, aes(data, vendite)) + geom_line() + scale_x_date('month')  + ylab("Daily Sales") +
-  xlab("")
-
-vendite1_day_pre
-
-
-# tentativo 4 ----
-# lisciamento esponenziale 
 # https://webs.rete.toscana.it/lso/c/document_library/get_file?uuid=dcfe244a-3038-4e00-a2c6-fff1aa19be72&groupId=38526
 
-# tentativo 4 ----
 
-library(tseries)
+
+# arima ----
+
 
 vendite1_day_pre
+
+vendite1_day_pre_split <- ts_split(vendite1_day_pre)
+
+train <- vendite1_day_pre_split$train
+autoplot(train)
+test <- vendite1_day_pre_split$test
+autoplot(test)
+
+
+arima1 <- auto.arima(train)
+
+predict(arima1,n.ahead = 50)
+
+
+
+
+
+
+
+
 vendite1_day_pre_diff <- diff(vendite1_day_pre)
 
 # visualize data
@@ -118,12 +53,9 @@ arima_diag(vendite1_day_pre)
 
 
 
-
 check_res(M4)
 
 
-# altri link da vedere ----
-# https://rpubs.com/riazakhan94/arima_with_example
 
 
 # materiale per valutazioni performance modelli ----
