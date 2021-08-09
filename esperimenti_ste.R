ac87dfa9db903d58c78c71e53748488b93c800bb
Stefano <marzoste3@gmail.com>
2021-08-09 11:33:46 +0200

    Aggiunta dei modelli con regressori

--- esperimenti_ste.R
+++ esperimenti_ste.R
@@ -1,4 +1,4 @@
-
+#### CESTINO ---------------------------------------------------------------------------
 ristorante1$tipo_giorno <- as.factor(ristorazione$tipo_giorno)
 ristorante1$weekday <- as.factor(ristorazione$weekday)
 ristorante1$holiday <- ristorazione$holiday
@@ -149,7 +149,7 @@ checkresiduals(M11)
 autoplot(forecast(M11, h=52, xreg = test_chiuso))+autolayer(test)
 
 
-## DEFINITIVO ----------------------------------------------------------------------------------
+## DEFINITIVO 
 
 library(tseries)
 library(urca)
@@ -207,7 +207,7 @@ checkresiduals(M1)
 
 #autoplot(forecast(M1, h=50))+autolayer(test)
 
-#USO AUTO ARIMA----------------------------------------------------------
+#USO AUTO ARIMA
 vendite1_sett_avg_pre_split_auto <- ts_split(vendite1_sett_avg_pre)
 
 # divisione in train e test
@@ -237,30 +237,7 @@ plot(forecast)
 lines(test_auto, col="red")
 legend("topleft",lty=1,bty = "n",col=c("red","blue"),c("testData","ARIMAPred"))
 
-## PROVA REGRESSORE------------------------------------------
-
-ristorante1_pre_covid_chiuso <- ristorante1 %>%
-  filter(ristorante1$data < reference_date) %>%
-  select(chiuso, data)
-
-chiuso_sett_avg_pre <- aggregate(chiuso ~ week_pre_covid, ristorante1_pre_covid_chiuso, mean)
-chiuso1_sett_avg_pre <- chiuso_sett_avg_pre$chiuso
-chiuso1_sett_avg_pre <- ts(chiuso1_sett_avg_pre,start=2017,frequency=52) 
-
-chiuso_split <- ts_split(chiuso1_sett_avg_pre)
-train_chiuso <- chiuso_split$train
-test_chiuso <- chiuso_split$test
-
-
-M3 <- auto.arima(train_auto, seasonal = T, xreg = train_chiuso)
-summary(M3)
-checkresiduals(M3)
-
-M3 %>%
-  forecast(h=50, xreg = test_chiuso) %>%  # h Number of periods for fore
-  autoplot() + autolayer(test_auto)
-
-###NUOVO MANUALE --------------------------------------------------------------
+###NUOVO MANUALE 
 
 #guardo acf e pacf del train
 
@@ -302,4 +279,149 @@ summary(M5)
 
 
 
+## PROVA REGRESSORE------------------------------------------
+
+ristorante1$top <- ristorazione$top
+
+ristorante1_pre_covid_top <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(top, data)
+
+top_sett_sum_pre <- aggregate(top ~ week_pre_covid, ristorante1_pre_covid_top, sum)
+top1_sett_sum_pre <- top_sett_sum_pre$top
+top1_sett_sum_pre <- ts(top1_sett_sum_pre,start=2017,frequency=52) 
+
+top_split <- ts_split(top1_sett_sum_pre)
+train_top <- as.factor(top_split$train)
+test_top <- top_split$test
+
+
+MR1 <- auto.arima(train_auto, seasonal = T, xreg = as.numeric(train_top))
+summary(MR1)
+
+pvalue = 2*pt(-350.8245/280.9505,115)
+
+
+ristorante1$pioggia <- ristorazione$pioggia
+
+ristorante1_pre_covid_pioggia <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(pioggia, data)
+
+pioggia_sett_avg_pre <- aggregate(pioggia ~ week_pre_covid, ristorante1_pre_covid_pioggia, sum)
+pioggia1_sett_avg_pre <- pioggia_sett_avg_pre$pioggia
+pioggia1_sett_avg_pre <- ts(pioggia1_sett_avg_pre,start=2017,frequency=52) 
+
+pioggia_split <- ts_split(pioggia1_sett_avg_pre)
+train_pioggia <- pioggia_split$train
+test_pioggia <- pioggia_split$test
+
+
+regr <- data.frame(train_top, train_pioggia)
+
+MR2 <- auto.arima(train_auto, seasonal = T, xreg = data.matrix(regr[,c("train_top", "train_pioggia")]))
+summary(MR2)
+
+
+MR3 <- auto.arima(train_auto, seasonal = T, xreg = train_pioggia)
+summary(MR3)
+
+ristorante1$vacanze_scuola <- as.numeric(ristorazione$`Vacanze scolastiche Lombardia`)
+
+ristorante1_pre_covid_vacanze_scuola <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(vacanze_scuola, data)
+
+vacanze_scuola_sett_avg_pre <- aggregate(vacanze_scuola ~ week_pre_covid, ristorante1_pre_covid_vacanze_scuola, sum)
+vacanze_scuola1_sett_avg_pre <- vacanze_scuola_sett_avg_pre$vacanze_scuola
+vacanze_scuola1_sett_avg_pre <- ts(vacanze_scuola1_sett_avg_pre,start=2017,frequency=52) 
+
+vacanze_scuola_split <- ts_split(vacanze_scuola1_sett_avg_pre)
+train_vacanze_scuola <- vacanze_scuola_split$train
+test_vacanze_scuola <- vacanze_scuola_split$test
+
+regr <- data.frame(train_chiuso, train_vacanze_scuola)
+
+
+
+ristorante1_pre_covid_chiuso <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(chiuso, data)
+
+chiuso_sett_avg_pre <- aggregate(chiuso ~ week_pre_covid, ristorante1_pre_covid_chiuso, sum)
+chiuso1_sett_avg_pre <- chiuso_sett_avg_pre$chiuso
+chiuso1_sett_avg_pre <- ts(chiuso1_sett_avg_pre,start=2017,frequency=52) 
+
+chiuso_split <- ts_split(chiuso1_sett_avg_pre)
+train_chiuso <- chiuso_split$train
+test_chiuso <- chiuso_split$test
+
+
+
+MR4 <- auto.arima(train_auto, D=1, xreg = train_chiuso)
+summary(MR4)
+
+pvalue = 2*pt(-740.1464/60.7140,115)
+
+ristorante1$fuori <- ristorazione$meteo_aperto
+
+ristorante1_pre_covid_fuori <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(fuori, data)
+
+fuori_sett_avg_pre <- aggregate(fuori ~ week_pre_covid, ristorante1_pre_covid_fuori, sum)
+fuori1_sett_avg_pre <- fuori_sett_avg_pre$fuori
+fuori1_sett_avg_pre <- ts(fuori1_sett_avg_pre,start=2017,frequency=52) 
+
+fuori_split <- ts_split(fuori1_sett_avg_pre)
+train_fuori <- fuori_split$train
+test_fuori <- fuori_split$test
+
+
+MR5 <- auto.arima(train_auto, D=1, xreg = train_fuori)
+summary(MR5)
+
+
+meteo <- read_delim("meteo2.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+meteo <- meteo[, -c(1)]
+meteo$data<-as.Date(meteo$data, format = "%d/%m/%Y")
+ristorazione<-merge(x=ristorazione,y=meteo,by="data",all.x=TRUE)
+
+
+ristorante1$neve <- as.numeric(ristorazione$neve)
+ristorante1_pre_covid_neve <- ristorante1 %>%
+  filter(ristorante1$data < reference_date) %>%
+  select(neve, data)
+
+neve_sett_avg_pre <- aggregate(neve ~ week_pre_covid, ristorante1_pre_covid_neve, sum)
+neve1_sett_avg_pre <- neve_sett_avg_pre$neve
+neve1_sett_avg_pre <- ts(neve1_sett_avg_pre,start=2017,frequency=52) 
+
+neve_split <- ts_split(neve1_sett_avg_pre)
+train_neve <- neve_split$train
+test_neve <- neve_split$test
+
+MR6 <- auto.arima(train_auto, D=1, xreg = train_neve)
+summary(MR6)
+
+pvalue = 2*pt(-825.0664/115.4211,115)
+pvalue
+
+
+MR6 %>%
+  forecast(h=50, xreg= test_neve) %>%  # h Number of periods for forecasting
+  autoplot() + autolayer(test_auto)
+
+
+forecast <- MR6 %>%
+  forecast(h=10, xreg= test_neve) 
+
+# alternativa per vedere previsioni con test set
+par(mfrow=c(1,1))
+plot(forecast)
+lines(test_auto, col="red")
+legend("topleft",lty=1,bty = "n",col=c("red","blue"),c("testData","ARIMAPred"))
+
+# valutazione qualità previsioni
+accuracy(forecast, test_auto[1:10])
 
