4bf94037a7c8c01e38085d5429278fb0184bc56f
lorenzlorg <lorenzlorg@gmail.com>
2021-08-12 10:23:58 +0200

    eliminazione commenti

--- esperimenti_lor.R
+++ esperimenti_lor.R
@@ -6,6 +6,32 @@
 # https://webs.rete.toscana.it/lso/c/document_library/get_file?uuid=dcfe244a-3038-4e00-a2c6-fff1aa19be72&groupId=38526
 
 
+# confronto estati ----
+
+# si procede con un'analisi più approfondita e più tencica del grafico per rispondere 
+# alla domanda di ricerca: ovvero nel 2020 c'era pochissime restrizioni, quasi 
+# zero però si vuole dimostrare che non è stata un'estate normale come lo può 
+# essere quella del 2019
+
+# esempio analisi:
+
+# Just came across this. Your first answer us plotting g the two sets the same 
+# scale (timewise) to see the differences visually. You have done this and can easily 
+# see there are some glaring differences. The next step is to use simple correlation 
+# analysis...and see how well are they related using the correlation coefficient (r). 
+# If the r is small your conclusion would be that they are weakly related and so no 
+# desirable comparisons and a larger value if r would suggest good comparisons s between 
+# the two series. The third step where there is good correlation is to test the statistical 
+# significance of the r. Here you can use the Shapiro Welch test which would assume the two 
+# series are normally distributed (null hypothesis ) or not (alternative hypothesis). 
+# There are other tests you can do but let me hope my answer helps.
+
+# To compare two time series simply estimate the COMMON appropriate arima model 
+# for each time series separately AND then estimate it globally ( putting the second 
+# series behind the first ) . Make sure that your software recognizes the beginning 
+# of the scond series and doesn't forecast it from the latter values of the first series. 
+# Perform an F test ala G. Chow to test the hypothesis of a common set of parameters. AUTOBOX , 
+# a program that I am involved with allows this test to be performed. SPSS may not.
 
 
 
--- restaurant_analysis.r
+++ restaurant_analysis.r
@@ -19,7 +19,7 @@ invisible(lapply(packages, library, character.only = TRUE))
 # setting working directory
 #working_dir = "C:/Users/Stefano/Documents/progetto_dslab/dati"
 #working_dir = "C:/Users/Lorenzo/Desktop/Progetto ds lab/progetto_dslab/dati"
-working_dir = "~/Desktop/Progetto ds lab/progetto_dslab/dati"
+working_dir = "~/Desktop/progetti uni github/progetto_dslab/dati"
 setwd(working_dir)
 
 # lettura dataset
@@ -800,29 +800,3 @@ source("~/Desktop/Progetto ds lab/progetto_dslab/other_scripts/confronti_estati_
 
 # confronto estati ristorante 6
 source("~/Desktop/Progetto ds lab/progetto_dslab/other_scripts/confronti_estati_ristoranti/confronto_estati_rist_6.R")
-
-## alcuni spunti ----
-
-# si procede con un'analisi più approfondita e più tencica del grafico per rispondere 
-# alla domanda di ricerca: ovvero nel 2020 c'era pochissime restrizioni, quasi 
-# zero però si vuole dimostrare che non è stata un'estate normale come lo può 
-# essere quella del 2019
-
-# esempio analisi:
-# Just came across this. Your first answer us plotting g the two sets the same 
-# scale (timewise) to see the differences visually. You have done this and can easily 
-# see there are some glaring differences. The next step is to use simple correlation 
-# analysis...and see how well are they related using the correlation coefficient (r). 
-# If the r is small your conclusion would be that they are weakly related and so no 
-# desirable comparisons and a larger value if r would suggest good comparisons s between 
-# the two series. The third step where there is good correlation is to test the statistical 
-# significance of the r. Here you can use the Shapiro Welch test which would assume the two 
-# series are normally distributed (null hypothesis ) or not (alternative hypothesis). 
-# There are other tests you can do but let me hope my answer helps.
-
-# To compare two time series simply estimate the COMMON appropriate arima model 
-# for each time series separately AND then estimate it globally ( putting the second 
-# series behind the first ) . Make sure that your software recognizes the beginning 
-# of the scond series and doesn't forecast it from the latter values of the first series. 
-# Perform an F test ala G. Chow to test the hypothesis of a common set of parameters. AUTOBOX , 
-# a program that I am involved with allows this test to be performed. SPSS may not.
