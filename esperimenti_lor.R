8125d3f11a751ea0c82f22d68f327d0fc3bf64e6
lorenzlorg <lorenzlorg@gmail.com>
2021-08-13 16:47:49 +0200

    sist brutta

--- esperimenti_lor.R
+++ esperimenti_lor.R
@@ -1,11 +1,5 @@
 # WORKING IN PROGRESS #
 
-
-# lisciamento esponenziale ----
-
-# https://webs.rete.toscana.it/lso/c/document_library/get_file?uuid=dcfe244a-3038-4e00-a2c6-fff1aa19be72&groupId=38526
-
-
 # confronto estati ----
 
 # si procede con un'analisi più approfondita e più tencica del grafico per rispondere 
