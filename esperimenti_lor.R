b483b9c7ce9dec545a38a16927e10af7736c1328
Lorenzo <lorenzlorg@gmail.com>
2021-09-09 19:00:14 +0200

    eliminazione commenti esp lor

--- esperimenti_lor.R
+++ esperimenti_lor.R
@@ -1,16 +1,4 @@
-# WORKING IN PROGRESS #
 
-# arima con regressori diversi ----
-
-# si può valutare anche modello SARIMA e VAR (per gestire + serie storiche)
-
-# bisogna provare con altre variabili: tipo is weekend, is holiday, 
-# noi utilizziamo ad ora solo variabili che sono legate al covid, non è troppo 
-# rigida questa scelta? I colori della zone sono abbastanza limitati, partendo da 
-# novembre essi agiscono su una piccola porzione di dati
-
-# ricordarsi come nel random forest modificare periodi pre e post covid, il periodo
-# pre dovrebbe fermarmi un pochino prima del picco
 
 
 
